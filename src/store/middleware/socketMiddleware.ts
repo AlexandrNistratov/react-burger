3b88ee429310ac5878b3762c228d788f9835856e
AlexandrNistratov <koss.1991@yandex.ru>
2022-07-08 11:39:43 +0300

    Починил редакс

--- src/store/middleware/socketMiddleware.ts
+++ src/store/middleware/socketMiddleware.ts
@@ -7,9 +7,9 @@ export const socketMiddleware = (wsUrl: string, wsActions: TwsActions): Middlewa
         let socket: WebSocket | null = null;
 
         return (next) => (action) => {
-            const {dispatch} = store;
-            const {type, payload} = action;
-            const {start} = RootSocketAction;
+            const { dispatch } = store;
+            const { type, payload } = action;
+            const { start, closed } = RootSocketAction;
 
             if (type === start) {
                 socket = new WebSocket(`${wsUrl}/all`);
@@ -18,32 +18,32 @@ export const socketMiddleware = (wsUrl: string, wsActions: TwsActions): Middlewa
                 const url = `${wsUrl}?token=${payload.token?.split("Bearer ").join("")}`;
                 console.log(url)
                 socket = new WebSocket(url);
-
-                if (socket) {
-                    socket.onopen = (event) => {
-                        console.log("подключились")
-                        dispatch(socketActionCreators.success());
-                    };
-
-                    socket.onerror = (event) => {
-                        console.log('ошибочка')
-                        dispatch(socketActionCreators.error());
-                    };
-
-                    socket.onmessage = (event) => {
-                        console.log('получили')
-                        const data = JSON.parse(event.data);
-                        console.log(data)
-                        dispatch(socketActionCreators.getMessage(data));
-                    };
-
-                    socket.onclose = (event) => {
-                        console.log('отключились')
-                        dispatch(socketActionCreators.close());
-                    };
-                }
-                next(action);
             }
+
+            if (socket) {
+                socket.onopen = (event) => {
+                    console.log("подключились")
+                    dispatch(socketActionCreators.success());
+                };
+
+                socket.onerror = (event) => {
+                    console.log('ошибочка')
+                    dispatch(socketActionCreators.error());
+                };
+
+                socket.onmessage = (event) => {
+                    console.log('получили')
+                    const data = JSON.parse(event.data);
+                    console.log(data)
+                    dispatch(socketActionCreators.getMessage(data));
+                };
+
+                socket.onclose = (event) => {
+                    console.log('отключились')
+                    dispatch(socketActionCreators.close());
+                };
+            }
+            next(action);
         }
     }
 };
